24d7b2913f6e907c5fe10665aa6048d2ccc57231
mahfuzmunna <me@mahfuzmunna.com>
2025-08-23 16:40:38 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/mahfuzmunna/recipeo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mahfuzmunna.recipeo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:recipeo_flutter/app/app.dart';
+import 'package:recipeo_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
